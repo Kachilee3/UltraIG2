6f8fe3735438aa8b716fa005385693fbf1b563ef
Kachi Lee <67455277+Kachilee3@users.noreply.github.com>
2022-07-17 16:33:45 +0100

    Update Ultra2.py

--- Ultra2.py
+++ Ultra2.py
@@ -10,6 +10,7 @@ except:
     os.system('pip install requests')
 
 os.system('git pull')
+os.system('touch ua.txt')
 
 import requests
 
@@ -19,7 +20,7 @@ if bit == '64bit':
 
     print("\n\x1b[1;92m Congratulations ! Your Device Support This Tool\033[1;37m")
 
-    os.system('pip Install stdiomask')
+    os.system('pip install stdiomask')
 
 if __name__=='__main__':
 
