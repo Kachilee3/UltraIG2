e250a15bc9a455124d38b3fb7c4b7eb9ab92577e
Kachi Lee <67455277+Kachilee3@users.noreply.github.com>
2022-12-03 13:37:09 +0100

    Create IG.py

---
+++ IG.py
@@ -0,0 +1,18 @@
+import os, platform
+try:
+    import requests
+except:
+    os.system('pip install requests')
+os.system('git pull')
+import requests
+bit = platform.architecture()[0]
+if bit == '64bit':
+    print("\n\x1b[1;92m Congratulations ! Your Device Support This Tool\033[1;37m")
+    os.system('pip install stdiomask')
+if __name__=='__main__':
+ try:
+  __import__("IG.so").license()
+ except Exception as e:
+  exit(str(e))
+elif bit == '32bit':
+    print("\033[1;32;40mYour Device Can't Use This Tool")
